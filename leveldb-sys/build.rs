9dbba94714fb39d084784b840557f84ef41c8005
Gleb Pomykalov <gleb@lancastr.com>
2019-01-13 16:21:40 +0300

    add linking with stdc++

--- leveldb-sys/build.rs
+++ leveldb-sys/build.rs
@@ -3,17 +3,23 @@ extern crate pkg_config;
 use std::env;
 
 fn main() {
+    let mode = match env::var_os("LEVELDB_STATIC") {
+        Some(_) => "static",
+        None => "dylib",
+    };
     println!("cargo:rerun-if-env-changed=LEVELDB_LIB_DIR");
     println!("cargo:rerun-if-env-changed=LEVELDB_STATIC");
     if let Ok(lib_dir) = env::var("LEVELDB_LIB_DIR") {
         println!("cargo:rustc-link-search=native={}", lib_dir);
-
-        let mode = match env::var_os("LEVELDB_STATIC") {
-            Some(_) => "static",
-            None => "dylib",
-        };
         println!("cargo:rustc-link-lib={0}=leveldb", mode);
     } else {
         pkg_config::find_library("libleveldb").unwrap();
     }
+    let target = env::var("TARGET").unwrap();
+    if target.contains("apple") {
+        println!("cargo:rustc-link-lib=c++");
+    } else {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
+
 }
